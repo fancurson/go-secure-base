574c148738bb39775fbec62ff3041685600b3ca4
fancurson <fancurson@gmail.com>
2026-02-27 22:05:52 +0300

    ci: refactore linter and main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,5 @@
+// Package main is the entry point for the GoSecure-Base microservice.
+// It initializes the server, security middlewares, and handles graceful shutdown.
 package main
 
 import (
@@ -9,12 +11,12 @@ import (
 )
 
 const (
-	HttpReadTimeOut  = 5 * time.Second
-	HttpWriteTimeout = 10 * time.Second
-	HttpIdleTimeout  = 120 * time.Second
+	HTTPReadTimeOut  = 5 * time.Second
+	HTTPWriteTimeout = 10 * time.Second
+	HTTPIdleTimeout  = 120 * time.Second
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
+func greet(w http.ResponseWriter, _ *http.Request) {
 	// Безопасность: предотвращаем sniffing атак, явно указывая тип
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	_, err := fmt.Fprintf(w, "GoSecure-Base: Authorized Access at %s", time.Now().Format(time.RFC3339))
@@ -31,9 +33,9 @@ func main() {
 	server := &http.Server{
 		Addr:         ":8080",
 		Handler:      mux,
-		ReadTimeout:  HttpReadTimeOut,  // Ограничение времени на чтение запроса
-		WriteTimeout: HttpWriteTimeout, // Ограничение времени на запись ответа
-		IdleTimeout:  HttpIdleTimeout,
+		ReadTimeout:  HTTPReadTimeOut,  // Ограничение времени на чтение запроса
+		WriteTimeout: HTTPWriteTimeout, // Ограничение времени на запись ответа
+		IdleTimeout:  HTTPIdleTimeout,
 	}
 
 	log.Println("Server starting on :8080...")

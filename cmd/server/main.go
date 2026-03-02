f910668d62fa2d7e489e7f6becfd3ed3358d8ec0
fancurson <fancurson@gmail.com>
2026-03-02 17:59:15 +0300

    feat: implement standard project layout and graceful shutdown

---
+++ cmd/server/main.go
@@ -0,0 +1,17 @@
+// Package main is the entry point for the GoSecure-Base microservice.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/fancurson/go-secure-base/internal/app"
+)
+
+func main() {
+	// В будущем тут может быть парсинг флагов
+	if err := app.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "application failed: %v\n", err)
+		os.Exit(1)
+	}
+}
